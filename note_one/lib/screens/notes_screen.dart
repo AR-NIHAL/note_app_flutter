351a3085bc3ed9a6cfd64c3002d23496e9ca3a40
Ashikur Rahman Nihal <arnihal.cs@gmail.com>
2025-10-07 22:56:22 +0600

    floating
    again

---
+++ note_one/lib/screens/notes_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:note_one/database/notes_database.dart';
+
+class NotesScreen extends StatefulWidget {
+  const NotesScreen({super.key});
+
+  @override
+  State<NotesScreen> createState() => _NotesScreenState();
+}
+
+class _NotesScreenState extends State<NotesScreen> {
+  List<Map<String, dynamic>> notes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchNotes();
+  }
+
+  Future<void> fetchNotes() async {
+    final fetchNotes = await NotesDatabase.instance.getNotes();
+
+    setState(() {
+      notes = fetchNotes;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text('NotesApp'),
+      ),
+
+      body: notes.isEmpty
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.note_add_outlined,
+                    size: 80,
+                    color: Colors.grey[600],
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  Text('No Notes Found ', style: TextStyle(fontSize: 20)),
+                ],
+              ),
+            )
+          : Padding(
+              padding: const EdgeInsets.all(16),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  childAspectRatio: 0.85,
+                ),
+                itemCount: notes.length,
+                itemBuilder: (context, index) {
+                  final note = notes[index];
+
+                  return Text(note['title']);
+                },
+              ),
+            ),
+    );
+  }
+}
